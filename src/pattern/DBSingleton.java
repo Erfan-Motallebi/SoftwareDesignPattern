751602e4ed4b07797233b9878fe82f668c4d00a1
Erfan Motallebi <FS.C0d3rZ@gmail.com>
2022-08-26 15:06:20 +0430

    added the method I with various approaches

--- src/pattern/DBSingleton.java
+++ src/pattern/DBSingleton.java
@@ -1,21 +1,52 @@
 package pattern;
 
-
 // Method I - Lazy Instantiation
 
-public class DBSingleton {
+//public class DBSingleton {
+//
+//		private static IDataAccess _dbSingleton;
+//		
+//		private DBSingleton() {
+//			// TODO Auto-generated constructor stub
+//		}
+//		
+//		public static IDataAccess getDBInstance() {
+//			if(_dbSingleton == null) {
+//				return new DataAccess();
+//			} else {
+//				return _dbSingleton;
+//			}
+//		}
+//}
 
-		private static IDataAccess _dbSingleton;
-		
-		private DBSingleton() {
-			// TODO Auto-generated constructor stub
-		}
-		
-		public static IDataAccess getDBInstance() {
-			if(_dbSingleton == null) {
-				return new DataAccess();
-			} else {
-				return _dbSingleton;
-			}
-		}
+// Method II - Synchronization in Java [ Thread-safe Approach I ]
+
+public class DBSingleton {
+	private static IDataAccess _dbSingleton;
+
+	private DBSingleton() {
+		// TODO Auto-generated constructor stub
+	}
+
+//	Synchronized Method - I
+//	public static synchronized IDataAccess getDBInstance() {
+//		if (_dbSingleton == null) {
+//			return new DataAccess();
+//		}
+//		return _dbSingleton;
+//	}
+
+//	Synchronized Block - II
+//	public static IDataAccess getDBInstance() {
+//		if (_dbSingleton == null) {
+//			synchronized (DBSingleton.class) {
+//				if (_dbSingleton == null) {
+//					_dbSingleton = new DataAccess();
+//				}
+//			}
+//		}
+//		return _dbSingleton;
+//
+//	}
+
 }

ce67c6ea918200884a1c0e28bfe83e716476c77e
Erfan Motallebi <FS.C0d3rZ@gmail.com>
2022-08-26 15:11:03 +0430

    added the third method of implementing the Singleton Pattern

--- src/pattern/DBSingleton.java
+++ src/pattern/DBSingleton.java
@@ -21,32 +21,47 @@ package pattern;
 
 // Method II - Synchronization in Java [ Thread-safe Approach I ]
 
-public class DBSingleton {
-	private static IDataAccess _dbSingleton;
-
-	private DBSingleton() {
-		// TODO Auto-generated constructor stub
-	}
-
-//	Synchronized Method - I
-//	public static synchronized IDataAccess getDBInstance() {
-//		if (_dbSingleton == null) {
-//			return new DataAccess();
-//		}
-//		return _dbSingleton;
-//	}
-
-//	Synchronized Block - II
-//	public static IDataAccess getDBInstance() {
-//		if (_dbSingleton == null) {
-//			synchronized (DBSingleton.class) {
-//				if (_dbSingleton == null) {
-//					_dbSingleton = new DataAccess();
-//				}
-//			}
-//		}
-//		return _dbSingleton;
+//public class DBSingleton {
+//	private static IDataAccess _dbSingleton;
 //
+//	private DBSingleton() {
+//		// TODO Auto-generated constructor stub
 //	}
+//
+////	Synchronized Method - I
+////	public static synchronized IDataAccess getDBInstance() {
+////		if (_dbSingleton == null) {
+////			return new DataAccess();
+////		}
+////		return _dbSingleton;
+////	}
+//
+////	Synchronized Block - II
+////	public static IDataAccess getDBInstance() {
+////		if (_dbSingleton == null) {
+////			synchronized (DBSingleton.class) {
+////				if (_dbSingleton == null) {
+////					_dbSingleton = new DataAccess();
+////				}
+////			}
+////		}
+////		return _dbSingleton;
+////
+////	}
+//
+//}
 
+
+// Method II - Eager Instantiation
+
+public class DBSingleton {
+	
+	private static IDataAccess _dbSingleton = new DataAccess();
+	
+	public static IDataAccess getDBInstance() {
+		if (_dbSingleton == null) {
+			return _dbSingleton;
+		}
+		return _dbSingleton;
+	}
 }

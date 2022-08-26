4c7b2a22951389867e957d5b433b47d514f3cb8b
Erfan Motallebi <FS.C0d3rZ@gmail.com>
2022-08-26 15:19:25 +0430

    The forth method of implementing the Singleton Pattern

--- src/pattern/DBSingleton.java
+++ src/pattern/DBSingleton.java
@@ -52,15 +52,33 @@ package pattern;
 //}
 
 
-// Method II - Eager Instantiation
+// Method III - Eager Instantiation
+
+//public class DBSingleton {
+//	
+//	private static IDataAccess _dbSingleton = new DataAccess();
+//	
+//	public static IDataAccess getDBInstance() {
+//		if (_dbSingleton == null) {
+//			return _dbSingleton;
+//		}
+//		return _dbSingleton;
+//	}
+//}
+
+
+// Method IV - Volatile - Double Check Locking
 
 public class DBSingleton {
-	
-	private static IDataAccess _dbSingleton = new DataAccess();
+	private static volatile IDataAccess _dbSingleton;
 	
 	public static IDataAccess getDBInstance() {
 		if (_dbSingleton == null) {
-			return _dbSingleton;
+			synchronized (DBSingleton.class) {
+				if (_dbSingleton == null) {
+					_dbSingleton = new DataAccess();
+				}
+			}
 		}
 		return _dbSingleton;
 	}
